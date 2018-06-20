565762236af7a74b14e96221712561bdae17212e
bfowler40 <bfowler40@gmail.com>
2018-06-20 17:28:44 +1000

    add solution for challenge #361 😎

--- challenges/361-tally-program.ts
+++ challenges/361-tally-program.ts
@@ -1,1 +1,34 @@
-console.log('challenge 361');
+/**
+ * Challenge #361 - Tally Program
+ * https://www.reddit.com/r/dailyprogrammer/comments/8jcffg/20180514_challenge_361_easy_tally_program/
+ */
+
+{
+	const matchReducer: any = (players: any, point: string) => {
+		const player = point.toLowerCase();
+
+		if (!players.hasOwnProperty(player)) {
+			Object.defineProperty(players, player, {
+				enumerable: true,
+				value: 0,
+				writable: true
+			});
+		}
+
+		// Add a point if lowercase or subtract if uppercase
+		player === point ? players[player]++ : players[player]--;
+
+		return players;
+	};
+
+	const matchResults: any = (input: string): string => {
+		const results = input
+			.split('')
+			.sort()
+			.reduce(matchReducer, {});
+
+		return JSON.stringify(results);
+	};
+
+	console.log(matchResults('EbAAdbBEaBaaBBdAccbeebaec'));
+}
